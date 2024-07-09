8d74876d8d13f92d3554f9bb494260104dedd737
DanielAntunes-dev <dap.junioruvv@gmail.com>
2024-07-09 16:19:33 -0300

    up: product whishlist

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -5,6 +5,7 @@ const {
   getAllProduct,
   updateProduct,
   deleteProduct,
+  addToWishlist,
 } = require("../controllers/productCtrl");
 const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
 
@@ -12,6 +13,7 @@ const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createProduct);
 router.get("/:id", getProduct);
+router.put("/wishlist", authMiddleware, addToWishlist);
 router.put("/:id", authMiddleware, isAdmin, updateProduct);
 router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
 router.get("/", getAllProduct);
